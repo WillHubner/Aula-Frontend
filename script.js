382476bfbb0792d9366dc43004ed84c5b4a4b164
WillHubner <willian.hubner@hotmail.co.uk>
2026-04-28 21:16:41 -0400

    Add Javascrit

---
+++ script.js
@@ -0,0 +1,10 @@
+botao = document.getElementById('btn-enviar');
+email = document.getElementById('ed-email');
+
+botao.addEventListener("click", 
+    function (){
+        alert(email.value)
+    }
+)
+
+
